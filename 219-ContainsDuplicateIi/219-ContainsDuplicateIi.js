b6b23dce9b6b6428c0d9fe42724640b7b481b212
Kothakota Abhinav <2003abhinavk@gmail.com>
2026-04-14 12:48:37 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.js
@@ -0,0 +1,20 @@
+// Last updated: 4/14/2026, 12:48:37 PM
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let seen = new Set()
+
+    for(let i = 0 ; i<nums.length;i++){
+        if(seen.has(nums[i])) return true
+
+        seen.add(nums[i])
+
+        if(seen.size > k){
+            seen.delete(nums[i-k])
+        }
+    }return false
+        
+};
